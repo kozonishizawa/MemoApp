1bc120df4bd56f12cdba3d94563f76d22ee121a7
西澤　康造 <Kozo@mba.local>
2021-05-23 01:43:12 +0900

    Extract components

--- App.jsx
+++ App.jsx
@@ -1,51 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
+import CircleButton from './src/components/CercleButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
 
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年5月21日 18:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
+      <AppBar />
+      <MemoList />
+      <CircleButton>+</CircleButton>
 
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年5月21日 18:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2021年5月21日 18:00</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-
-      </View>
-      <View style={styles.cercleButton}>
-        <Text style={styles.cercleButtonLabel}>+</Text>
-      </View>
     </View>
   );
 }
@@ -54,67 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    color: 'rgba(255, 255, 255, 0.8)',
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#ffffff',
-    fontWeight: 'bold',
-  },
-  memoListItem: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  cercleButton: {
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    width: 64,
-    height: 64,
-    backgroundColor: '#467Fd3',
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    elevation: 8,
-  },
-  cercleButtonLabel: {
-    color: '#ffffff',
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
---
+++ src/components/CercleButton.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { string } from 'prop-types';
+
+export default function CircleButton(props) {
+  const { children } = props;
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{ children }</Text>
+    </View>
+  );
+}
+
+CircleButton.propTypes = {
+  children: string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    width: 64,
+    height: 64,
+    backgroundColor: '#467Fd3',
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+
+});

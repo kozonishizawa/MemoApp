83b5971c819ae0d5490a47e5ee73467321197185
西澤　康造 <Kozo@mba.local>
2021-05-23 00:41:33 +0900

    Style MemoList screen

---
+++ src/components/Hello.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function Hello() {
+  return (
+    <View>
+      <Text>こんにちは！</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#ffffff',
+  },
+});
+export default Hello;
